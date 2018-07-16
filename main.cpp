6ff629852623599bde263ccc166879007d202155
Ian Klatzco <iklatzco@gmail.com>
2018-07-15 17:17:58 -0700

    arrows draw in center of screen

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 #include <iostream>
 
 #define ARROW_SIZE 64
-#define LEFT_ARROW 0
-#define DOWN_ARROW 1
-#define UP_ARROW 2
-#define RIGHT_ARROW 3
+#define LEFT 0
+#define DOWN 1
+#define UP 2
+#define RIGHT 3
+
+#define WINDOW_HEIGHT 480
+#define WINDOW_WIDTH 640
+#define WINDOW_Y_CENTER WINDOW_HEIGHT/2
+#define WINDOW_X_CENTER WINDOW_WIDTH/2
 
 int main()
 {
@@ -17,7 +22,7 @@ int main()
 	// To draw stuff, you can also use OpenGL directly and totally ignore
 	// the sfml-graphics module. sf::Window internally creates an OpenGL
 	// context and is ready to accept your OpenGL calls.
-	sf::RenderWindow window(sf::VideoMode(640, 480), "stepminia",
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "stepminia",
 		sf::Style::None, settings);
 
 	// we may want to offload the window drawing to a thread.
@@ -32,13 +37,18 @@ int main()
         return EXIT_FAILURE;
 
 	// we'll probably turn these into receptors eventually
-	sf::Sprite left(texture);
-    sf::Sprite down(texture);
-    sf::Sprite up(texture);
-	sf::Sprite right(texture);
+	sf::Sprite left(texture), down(texture), up(texture), right(texture);
 
 	// copied in
 	std::vector<sf::Sprite> arrow_vector = {left, down, up, right};
+
+	// set arrow positions
+	int offset = -ARROW_SIZE * 2;
+	for ( auto &sprite : arrow_vector )
+	{
+		sprite.setPosition(WINDOW_X_CENTER + offset, 32);
+		offset += ARROW_SIZE;
+	}
 
 	// from transformable.
 	// up.setRotation(150); // rotates around the origin, which is the
@@ -79,8 +89,18 @@ int main()
 			{
 				if (event.key.code == sf::Keyboard::Escape)
 					window.close();
-				if (event.key.code == sf::Keyboard::E)
-					down.move(sf::Vector2f(0, 10));
+
+				if (event.key.code == sf::Keyboard::I)
+					arrow_vector[LEFT].move(sf::Vector2f(0, 10));
+
+				if (event.key.code == sf::Keyboard::D)
+					arrow_vector[DOWN].move(sf::Vector2f(0, 10));
+
+				if (event.key.code == sf::Keyboard::F)
+					arrow_vector[UP].move(sf::Vector2f(0, 10));
+
+				if (event.key.code == sf::Keyboard::H)
+					arrow_vector[RIGHT].move(sf::Vector2f(0, 10));
 			}
 				
         }
@@ -91,9 +111,7 @@ int main()
 		int i = 0;
 		for ( auto &sprite : arrow_vector )
 		{
-			sprite.setPosition( ARROW_SIZE*i, 0 );
 			window.draw(sprite);
-			i++;
 		}
 
         // Draw the string
