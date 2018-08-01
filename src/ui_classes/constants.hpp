9a5372317875e3c418703546ddccc8fea424a5ec
Ian Klatzco <iklatzco@gmail.com>
2018-07-31 21:40:12 -0700

    make cmake work, add parens for some macros

--- src/ui_classes/constants.hpp
+++ src/ui_classes/constants.hpp
@@ -8,7 +8,9 @@
 
 #define WINDOW_HEIGHT 480
 #define WINDOW_WIDTH 640
-#define WINDOW_Y_CENTER WINDOW_HEIGHT / 2
-#define WINDOW_X_CENTER WINDOW_WIDTH / 2 
+
+// need to be wrapped in parens in case of order of operation issues
+#define WINDOW_Y_CENTER (WINDOW_HEIGHT / 2)
+#define WINDOW_X_CENTER (WINDOW_WIDTH / 2)
 
 #define OFFSET_FROM_TOP_OF_SCREEN 32
